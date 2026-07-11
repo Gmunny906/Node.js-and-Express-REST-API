1aa83cfda7f939592b57c1321280c3f13ddb0eb2
Grayson Siver <graysonsiver88@gmail.com>
2026-07-11 14:34:30 -0500

    Added the items file that will hold the //Database// and the routes. The base file setup for the file... (typing file over and over again)

---
+++ src/routes/items.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const router = express.Router();
+
+//let is used here so that items are not reassigned to a new array-
+// - but the items array can still be modified.
+//tldr; const prevents the reassignment of the array variable
+
+let items = [];
+
+    
+
+
+
+
+export default router;
