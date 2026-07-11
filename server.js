8cd0938687e809dc4d78c667c9ef1f32d5594a45
Grayson Siver <graysonsiver88@gmail.com>
2026-07-11 18:34:34 -0500

    Imported the router, added it to the app, added the global catch all

--- server.js
+++ server.js
@@ -5,10 +5,19 @@
 
 import express from "express";
 import { logger } from "./src/middleware/logger.js";
+import {router} from "./src/routes/items.js";
+
 
 const app = express();
 app.use(express.json());
 app.use(logger);
+app.use('/items', router);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
+
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
